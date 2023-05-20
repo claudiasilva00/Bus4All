9d2d20067540f169755919e27c985ad16560c6a9
Cláudia <al70759@utad.eu>
2023-05-20 22:07:17 +0100

    É BATATA MAS FUNCIONA , tabela de preços

--- Resources/Pages/Tarifas.xaml.cs
+++ Resources/Pages/Tarifas.xaml.cs
@@ -7,5 +7,14 @@ public partial class Tarifas : ContentPage
 		InitializeComponent();
 	}
 
+    private void ImageButton_Clicked(object sender, EventArgs e)
+    {
+        priceTableGrid.IsVisible = true;
 
+    }
+
+    private void ImageButton_Clicked_1(object sender, EventArgs e)
+    {
+        priceTableGrid.IsVisible = false;
+    }
 }
