a06215704cabe0084580e428acafbf34bb56e6c4
Pedro Barroso <al71134@alunos.utad.pt>
2023-05-20 18:08:11 +0100

    ok

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,15 +1,19 @@
-﻿using Bus4All;
-namespace Bus4All;
+﻿namespace Bus4All;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+        Routing.RegisterRoute(nameof(MainPage),typeof(MainPage));
         Routing.RegisterRoute("seta",typeof(Horarios));
         Routing.RegisterRoute("bus",typeof(Paragens));
         Routing.RegisterRoute("sinais",typeof(SelecionarPercurso));
         Routing.RegisterRoute("euro",typeof(Tarifas));
         Routing.RegisterRoute("map",typeof(Mapa));
+    }
+
+    private void ShellContent_Appearing(object sender,EventArgs e) {
+        Navigation.PopToRootAsync();
     }
 }
