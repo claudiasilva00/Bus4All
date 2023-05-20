f7b7e8d3893f4e7d53d2fe6827a46efc4cf0cd04
Cláudia <al70759@utad.eu>
2023-05-20 18:58:34 +0100

    mais trolhagem da seleçâo

--- Resources/Pages/Paragens.xaml.cs
+++ Resources/Pages/Paragens.xaml.cs
@@ -1,11 +1,44 @@
+using System.Runtime.InteropServices;
+using System;
+
 namespace Bus4All;
 
 public partial class Paragens : ContentPage
 {
-	public Paragens()
+
+    private List<string> paragemList;
+    public Paragens()
 	{
 		InitializeComponent();
-       
+
+        paragemList = new List<string>
+        {
+            "UTAD REITORIA",
+            "UTAD ENGENHARIAS",
+            "UTAD PEDAGOGICO",
+            "UTAD LIVRARIA",
+            "PRAÇA GALIZA",
+            "AV.UNIVERSIDADE",
+            "CARREIRA LONGA ",
+            "R.AUGUSTO CÉSAR",
+            "R.MANUEL SANTOS",
+            "ESCOLAS ARAUCÁR",
+            "IGREJA STº ANTÓ",
+            "C.SAUDE MATEUS ",
+            "R.GASPAR SAMEIR",
+            "R.GASPAR SAMEIR",
+            "MATEUS         ",
+            "Bº DO ALEM     ",
+            "ABAMBRES       ",
+            "COOPERATIVA    ",
+            "LUGAR DO BOQUE ",
+            "Bº STªA MARIA  ",
+            "RIO CORGO      ",
+            "CURVA DA TIMPEI",
+            "PR. 25 DE ABRIL",
+            "CEMITÉRIO      ",
+        };
+        
     }
-   
+    
 }
--- Resources/Pages/SelecionarPercurso.xaml.cs
+++ Resources/Pages/SelecionarPercurso.xaml.cs
@@ -4,19 +4,51 @@ namespace Bus4All;
 
 public partial class SelecionarPercurso : ContentPage
 {
-	public SelecionarPercurso()
+    private List<string> paragemList;
+    public SelecionarPercurso()
 	{
 		InitializeComponent();
-	}
 
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-        string embarqueText = pembarque.Text;
-        string desembarqueText = pdesembarque.Text;
+        paragemList = new List<string>
+        {
+            "UTAD REITORIA",
+            "UTAD ENGENHARIAS",
+            "UTAD PEDAGOGICO",
+            "UTAD LIVRARIA",
+            "PRAÇA GALIZA",
+            "AV.UNIVERSIDADE",
+            "CARREIRA LONGA ",
+            "R.AUGUSTO CÉSAR",
+            "R.MANUEL SANTOS",
+            "ESCOLAS ARAUCÁR",
+            "IGREJA STº ANTÓ",
+            "C.SAUDE MATEUS ",
+            "R.GASPAR SAMEIR",
+            "R.GASPAR SAMEIR",
+            "MATEUS         ",
+            "Bº DO ALEM     ",
+            "ABAMBRES       ",
+            "COOPERATIVA    ",
+            "LUGAR DO BOQUE ",
+            "Bº STªA MARIA  ",
+            "RIO CORGO      ",
+            "CURVA DA TIMPEI",
+            "PR. 25 DE ABRIL",
+            "CEMITÉRIO      ",
+        };
 
-        string message = $"Embarque: {embarqueText}\n\nDesembarque: {desembarqueText}";
+        pembarquePicker.ItemsSource = paragemList;
+        pdesembarquePicker.ItemsSource = paragemList;
 
-        DisplayAlert("PERCURSO SELECIONADO", message, "CONFIRMAR", "CANCELAR");
+    }
 
+
+  private void Button_Clicked(object sender, EventArgs e)
+   {
+        string selectedPembarque = pembarquePicker.SelectedItem as string;
+        string selectedPdesembarque = pdesembarquePicker.SelectedItem as string;
+
+            
+            DisplayAlert("PERCURSO SELECIONADO", $"Embarque: {selectedPembarque}\n\nDesembarque: {selectedPdesembarque}", "CONFIRMAR", "CANCELAR");
     }
 }
