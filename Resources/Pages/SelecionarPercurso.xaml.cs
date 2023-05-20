8782a366578db7de9b823626a9d612a2e157823e
Cláudia <al70759@utad.eu>
2023-05-20 18:06:29 +0100

    Selecionar Percurso com o pop up para confirmar

--- Resources/Pages/SelecionarPercurso.xaml.cs
+++ Resources/Pages/SelecionarPercurso.xaml.cs
@@ -11,6 +11,12 @@ public partial class SelecionarPercurso : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-		DisplayAlert("Percurso Selecionado","", "Confirmar");
+        string embarqueText = pembarque.Text;
+        string desembarqueText = pdesembarque.Text;
+
+        string message = $"Embarque: {embarqueText}\nDesembarque: {desembarqueText}";
+
+        DisplayAlert("PERCURSO SELECIONADO", message, "CONFIRMAR", "CANCELAR");
+
     }
 }
