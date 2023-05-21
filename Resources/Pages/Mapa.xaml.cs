b1d533adb1afaba948b2d52b0544d382567b7b01
Pedro Barroso <al71134@alunos.utad.pt>
2023-05-21 17:12:11 +0100

    fodasse

--- Resources/Pages/Mapa.xaml.cs
+++ Resources/Pages/Mapa.xaml.cs
@@ -10,9 +10,9 @@ public partial class Mapa : ContentPage
     private void ImageButton_Clicked(object sender,EventArgs e) {
 		DisplayAlert("Alerta","O seu Autocarro encontra-se a 2km de si","OK");
         Vibration.Default.Vibrate(1000);
-        Thread.Sleep(1000);
+        Thread.Sleep(1500);
         Vibration.Default.Vibrate(1000);
-        Thread.Sleep(1000);
+        Thread.Sleep(1500);
         //// DisplayAlert("Alerta","O seu Autocarro encontra-se a 300 m de si","OK");
         // Vibration.Default.Vibrate(100);
         // Vibration.Default.Vibrate(100);
