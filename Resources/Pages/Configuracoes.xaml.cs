1583c8fb6e3003dac277f2f2f1548ade38a823a1
Cláudia <al70759@utad.eu>
2023-05-21 20:11:44 +0100

    Eu levava estes botões num date, puta de LINDOS

--- Resources/Pages/Configuracoes.xaml.cs
+++ Resources/Pages/Configuracoes.xaml.cs
@@ -6,4 +6,49 @@ public partial class Configuracoes : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void Switch_Toggled(object sender, ToggledEventArgs e)
+    { Switch switchControl = (Switch)sender;
+        bool isOn = switchControl.IsToggled;
+
+        if (isOn)
+        {
+            // Change the switch color to green
+            switchControl.ThumbColor = Color.FromRgb(19, 192, 90);
+            switchControl.OnColor = Color.FromRgb(19, 192, 90);
+
+            // Perform actions for the "On" state
+        }
+        else
+        {
+            // Change the switch color to red
+            switchControl.ThumbColor = Color.FromRgb(145, 12, 12);
+            switchControl.OnColor = Color.FromRgb(145, 12, 12);
+
+            // Perform actions for the "Off" state
+        }
+    }
+    private void switchButtonn_Toggled(object sender, ToggledEventArgs e)
+    {
+        Switch switchControl = (Switch)sender;
+        bool isOn = switchControl.IsToggled;
+
+        if (isOn)
+        {
+            // Change the switch color to green
+            switchControl.ThumbColor = Color.FromRgb(19, 192, 90);
+            switchControl.OnColor = Color.FromRgb(19, 192, 90);
+
+            // Perform actions for the "On" state
+        }
+        else
+        {
+            // Change the switch color to red
+            switchControl.ThumbColor = Color.FromRgb(145, 12, 12);
+            switchControl.OnColor = Color.FromRgb(255, 0, 0);
+
+            // Perform actions for the "Off" state
+        }
+    }
+
+
 }
