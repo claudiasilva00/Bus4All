ef91d6f602285772090ab162b33b22541e82ac43
Cláudia <al70759@utad.eu>
2023-05-21 20:11:57 +0100

    second date?

--- Resources/Pages/Configuracoes.xaml.cs
+++ Resources/Pages/Configuracoes.xaml.cs
@@ -12,19 +12,19 @@ public partial class Configuracoes : ContentPage
 
         if (isOn)
         {
-            // Change the switch color to green
+            
             switchControl.ThumbColor = Color.FromRgb(19, 192, 90);
             switchControl.OnColor = Color.FromRgb(19, 192, 90);
 
-            // Perform actions for the "On" state
+            
         }
         else
         {
-            // Change the switch color to red
+           
             switchControl.ThumbColor = Color.FromRgb(145, 12, 12);
             switchControl.OnColor = Color.FromRgb(145, 12, 12);
 
-            // Perform actions for the "Off" state
+          
         }
     }
     private void switchButtonn_Toggled(object sender, ToggledEventArgs e)
@@ -34,19 +34,19 @@ public partial class Configuracoes : ContentPage
 
         if (isOn)
         {
-            // Change the switch color to green
+            
             switchControl.ThumbColor = Color.FromRgb(19, 192, 90);
             switchControl.OnColor = Color.FromRgb(19, 192, 90);
 
-            // Perform actions for the "On" state
+            
         }
         else
         {
-            // Change the switch color to red
+          
             switchControl.ThumbColor = Color.FromRgb(145, 12, 12);
             switchControl.OnColor = Color.FromRgb(255, 0, 0);
 
-            // Perform actions for the "Off" state
+        
         }
     }
 
