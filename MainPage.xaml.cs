5888af599c4b119dc90739f538f3f0940b69993f
Pedro Barroso <al71134@alunos.utad.pt>
2023-05-21 17:55:16 +0100

    XD

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,24 +14,24 @@ public partial class MainPage : ContentPage
         
     }
 
-    private void Seta_Clicked(object sender,EventArgs e) {
-        Shell.Current.GoToAsync("seta");
+    async private void Seta_Clicked(object sender,EventArgs e) {
+        await Shell.Current.GoToAsync("seta");
     }
 
-    private void Bus_Clicked(object sender,EventArgs e) {
-        Shell.Current.GoToAsync("bus");
+    async private void Bus_Clicked(object sender,EventArgs e) {
+        await Shell.Current.GoToAsync("bus");
     }
 
-    private void Euro_Clicked(object sender,EventArgs e) {
-        Shell.Current.GoToAsync("euro");
+    async private void Euro_Clicked(object sender,EventArgs e) {
+        await Shell.Current.GoToAsync("euro");
     }
 
-    private void Sinais_Clicked(object sender,EventArgs e) {
-        Shell.Current.GoToAsync("sinais");
+    async private void Sinais_Clicked(object sender,EventArgs e) {
+        await Shell.Current.GoToAsync("sinais");
     }
 
-    private void Mappoint_Clicked(object sender,EventArgs e) {
-        Shell.Current.GoToAsync("map");
+    async private void Mappoint_Clicked(object sender,EventArgs e) {
+        await Shell.Current.GoToAsync("map");
     }
 
     //private void OnCounterClicked(object sender, EventArgs e)
